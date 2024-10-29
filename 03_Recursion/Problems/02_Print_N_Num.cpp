4955a24e3a4083001d2afdd66a9612360cd5280b
Kasak Gupta <141016805+kasakgupta@users.noreply.github.com>
2024-10-29 23:39:23 +0530

    problem 2

--- 03_Recursion/Problems/02_Print_N_Num.cpp
+++ 03_Recursion/Problems/02_Print_N_Num.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void printName(int i, int n) {
+void printNum(int i, int n) {
     // Base case: If the current count `i` exceeds `n`, stop the recursion
     if (i > n) return;
 
     // Recursive case: Print the number and call the function with an incremented count
     cout << i<< endl;
-    printName(i + 1, n);  // Recursive call with `i` increased by 1
+    printNum(i + 1, n);  // Recursive call with `i` increased by 1
 }
 
 int main() {
@@ -16,7 +16,7 @@ int main() {
     cin >> n;
 
     // Initial call to the function, starting the count at 1
-    printName(1, n);
+    printNum(1, n);
 
     return 0;
 }
